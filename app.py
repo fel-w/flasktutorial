2bd25af819938970d11e2a8947c336699c716604
fel-w <wangottafelix@gmail.com>
2022-01-01 03:21:22 +0300

    Add database with sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+from sqlalchemy.orm import defaultload
 
 app = Flask(__name__)
+
+# Db location using relative path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+# Initializa database with settings from app
+db = SQLAlchemy(app)
+
+# Model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Function to return info upon update in db
+    def __repr__(self):
+        return '<Task %r>' %self.id
 
 @app.route('/')
 def index():
