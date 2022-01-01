09b8fa9e931de0ff33fcb9ca3113af94016827e2
fel-w <wangottafelix@gmail.com>
2022-01-01 04:40:55 +0300

    Add task creation logic

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-from sqlalchemy.orm import defaultload
+from sqlalchemy.orm import defaultload, exc
 
 app = Flask(__name__)
 
@@ -22,9 +22,22 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' %self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding the task'
+    else:
+        # Get all the contents in the database and store them in tasks
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
